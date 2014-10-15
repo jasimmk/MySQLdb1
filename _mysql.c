6ca70c8614b8dcd402e109de7069c7be6d017832
Jasim Muhammed <hello@jasimmk.com>
2014-10-15 09:43:22 +0530

    Fixing error: #error <my_config.h> MUST be included first!

--- _mysql.c
+++ _mysql.c
@@ -26,6 +26,13 @@ OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
 PERFORMANCE OF THIS SOFTWARE.
 */
 
+#if defined(MS_WINDOWS)
+#include <config-win.h>
+#else
+#include "my_config.h"
+#endif
+#include "mysql.h"
+#include "mysqld_error.h"
 #include "Python.h"
 #if PY_MAJOR_VERSION >= 3
 #define IS_PY3K
@@ -38,13 +45,6 @@ PERFORMANCE OF THIS SOFTWARE.
 #endif
 #include "pymemcompat.h"
 #include "structmember.h"
-#if defined(MS_WINDOWS)
-#include <config-win.h>
-#else
-#include "my_config.h"
-#endif
-#include "mysql.h"
-#include "mysqld_error.h"
 #include "errmsg.h"
 
 #if PY_VERSION_HEX < 0x02020000
